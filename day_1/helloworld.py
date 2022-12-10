42d219498ffe38f12391d52e1a85c05edcd349da
ValeriaReveles <valeriareveles12@gmail.com>
2022-12-10 14:53:26 -0600

    Python exercises for day 1 and 2

---
+++ day_1/helloworld.py
@@ -0,0 +1,45 @@
+# Day 1 - 30DaysOfPython Challenge:
+import sys
+import math
+
+
+# To print version from code editor, you need to write a python program:
+print("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+
+# Perform the following operations:
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# Write strings:
+print("Valeria")
+print("Reveles")
+print("US")
+print("I am enjoying 30 days of python")
+
+# Check the data types of following:
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Valeria', 'Python', 'US']))
+print(type("Valeria"))
+print(type("Reveles"))
+print(type("US"))
+
+# Exercise Level 2: Run Python file.
+# To do so, navigate to directory and then run the python file
+
+# Find the Euclidian distance between (2,3) and (10,8).
+firstPoint = [2, 3]
+secondPoint = [10, 8]
+
+# Use math.dis() method to calculate Euclidean distance:
+print(math.dist(firstPoint, secondPoint))
---
+++ day_2/variables.py
@@ -0,0 +1,19 @@
+# Day 2: 30 Days of python programming
+
+first_name = "Valeria"
+last_name = "Reveles"
+full_name = "Valeria Reveles"
+country = "US"
+city = "San Antonio"
+age = 29
+current_year = 2022
+is_married = False
+is_true = True
+is_light_on = True
+
+# Declare multiple variables in one line:
+
+first_name, last_name, full_name, country, age, is_married = 'Valeria', 'Reveles', 'Valeria Reveles', 'US', 29, False
+print(first_name, last_name, full_name, country, age, is_married )
+
+
